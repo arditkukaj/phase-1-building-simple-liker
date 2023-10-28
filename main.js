d3d39aab1f86513aa589988ca18daba1b826ff39
arditkukaj <arditkukaj4@gmail.com>
2023-10-28 17:07:24 -0400

    Completed lab

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+const colorStates = {
+  "red" : "",
+  "": "red"
+};
 
 // Your JavaScript code goes here!
 
 
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert("You notified the server!");
+      alert(serverMessage);
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      const div = document.querySelector("modal");
+      div.classList.remove("hidden");
 
+      setTimeout(() => {
+        const div = document.querySelector("modal");
+        div.classList.add("hidden");
+      }, 3000);
+    
+    });
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+ }
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
